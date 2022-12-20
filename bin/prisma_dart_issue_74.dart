28c36fd2c1f0363cd762d35725350c196ddb3497
Seven Du <whine.whitish.0u@icloud.com>
2022-12-20 14:07:28 +0800

    Initial commit

---
+++ bin/prisma_dart_issue_74.dart
@@ -0,0 +1,5 @@
+import 'package:prisma_dart_issue_74/prisma_dart_issue_74.dart' as prisma_dart_issue_74;
+
+void main(List<String> arguments) {
+  print('Hello world: ${prisma_dart_issue_74.calculate()}!');
+}
