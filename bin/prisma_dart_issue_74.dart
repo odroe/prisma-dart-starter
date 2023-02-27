246016017a34d2076d03f6624926866dcc44b865
Seven Du <whine.whitish.0u@icloud.com>
2023-02-27 19:50:37 +0800

    chore: *

--- bin/prisma_dart_issue_74.dart
+++
@@ -1,5 +0,0 @@
-import 'package:prisma_dart_issue_74/prisma_dart_issue_74.dart' as prisma_dart_issue_74;
-
-void main(List<String> arguments) {
-  print('Hello world: ${prisma_dart_issue_74.calculate()}!');
-}
--- lib/prisma_configurator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:orm/configure.dart';
-
-/// Configure Prisma for production environment.
-/// 
-/// **NOTE**: The function name must be configurePrisma.
-void configurePrisma(PrismaEnvironment environment) {
-  environment['DATABASE_URL'] = r'postgres://postgres:postgres@localhost:5432/postgres?schema=public';
-}
